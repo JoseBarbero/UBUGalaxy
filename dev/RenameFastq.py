bf2289087d0e67290e872c6b4e3813efb0f8abec
JoseBarbero <jabarbero@ubu.es>
2019-08-01 11:09:04 +0200

    New rename fastq utility.

--- dev/RenameFastq.py
+++ dev/RenameFastq.py
@@ -1,16 +1,19 @@
 import os
-
+import sys
 
 # Function to rename multiple files
 def rename():
-    i = 0
-    for directory in ["Forward", "Reverse"]:
-        for filename in os.listdir(directory):
-            name = filename.split(".")[0]
-            extension = filename.split(".")[1:]
-            os.rename(directory+"/"+filename, directory+"/"+name.split("_")[0]+"_"+name.split("_")[-2]+"."+".".join(extension))
+	if len(sys.argv) > 1:
+		if sys.argv[1] == "help":
+			print("Usage: \n Write \t renamefastq *directory path* \t and every fastq file in this directory will be renamed.")
+			print("Example: \n renamefastq ./Forward \t will rename every file in Forward directory")
+		else:
+			for filename in os.listdir(sys.argv[1]):
+				name = filename.split(".")[0]
+				extension = filename.split(".")[1:]
+				os.rename(sys.argv[1]+"/"+filename, sys.argv[1]+"/"+name.split("_")[0]+"_"+name.split("_")[-2]+"."+".".join(extension))
+	else:
+		print("You need to provide a directory route.")
 
-# Driver Code
 if __name__ == '__main__':
-    # Calling main() function
-    rename()
+	rename()
